de7a3c3f1eb8296f947db2f8be8a005e898600ee
Osman Homek <osman@geonni.com>
2016-10-07 07:20:38 +0300

    second update

--- controllers/section.go
+++ controllers/section.go
@@ -5,6 +5,7 @@ import (
 	//"encoding/json"
 	"fmt"
 	"log"
+
 	"github.com/gin-gonic/gin"
 	"gopkg.in/mgo.v2"
 	//"gopkg.in/mgo.v2/bson"
@@ -23,43 +24,50 @@ func NewSectionController(s *mgo.Session) *SectionControllerImpl {
 	return &SectionControllerImpl{s}
 }
 
-type Sections struct {  
-    orderIndex    int   `json:"orderIndex"`
-    engName   string   `json:"engName"`
-    trName string `json:"trName"`
-    avatar   string   `json:"avatar"`
-}
+/*
+type (
+	// Sections represents the structure of our resource
+	Sections struct {
+		orderIndex int    `json:"orderIndex"`
+		engName    string `json:"engName"`
+		trName     string `json:"trName"`
+		avatar     string `json:"avatar"`
+	}
+)
+*/
 
 // GetSections retrieves an individual user resource
 func (uc SectionControllerImpl) GetSections(c *gin.Context) {
 
-	u := make([]models.Section,0,3)
-	//result := make([]models.User, 0, 10) 
+	//u := make([]models.Section, 0, 3)
+	u := models.Section{}
+	//result := make([]models.User, 0, 10)
 	//err := uc.session.DB("gunde5dkcom").C("sections").Find(bson.M{"engName": "fruits"}).Sort("orderIndex").All(&u)
-	err := uc.session.DB("gunde5dkcom").C("sections").Find(nil).Sort("orderIndex").All(&u)
+	//err := uc.session.DB("gunde5dkcom").C("sections").Find(nil).Sort("orderIndex").All(&u)
+	err := uc.session.DB("gunde5dkcom").C("sections").Find(nil).Sort("orderIndex").One(&u)
 	if err != nil {
 		log.Println("Failed get all sections: ", err)
-        return
+		return
 	}
 	fmt.Println(u)
 
 	/*
-	sessionc := uc.session.Copy()
-	defer sessionc.Close()
-	c := sessionc.DB("gunde5dkcom").C("sections")
+			sessionc := uc.session.Copy()
+			defer sessionc.Close()
+			c := sessionc.DB("gunde5dkcom").C("sections")
 
-	var sections []Sections
-	err := c.Find(bson.M{}).All(&sections)  
-	if err != nil {
-		log.Println("Failed get all books: ", err)
-        return
-	}
+			var sections []Sections
+			err := c.Find(bson.M{}).All(&sections)
+			if err != nil {
+				log.Println("Failed get all books: ", err)
+		        return
+			}
 
-	respBody, err := json.MarshalIndent(sections, "", "  ")
-	if err != nil {
-    	log.Println("Failed get all books: ", err)
-	}	
-	fmt.Println(respBody)
+			respBody, err := json.MarshalIndent(sections, "", "  ")
+			if err != nil {
+		    	log.Println("Failed get all books: ", err)
+			}
+			fmt.Println(respBody)
 	*/
 	fmt.Println("fmt")
 	log.Println("log")
--- models/section.go
+++ models/section.go
@@ -3,12 +3,12 @@ package models
 import "gopkg.in/mgo.v2/bson"
 
 type (
-	// User represents the structure of our resource
+	// Section represents the structure of our resource
 	Section struct {
-		Id      bson.ObjectId `json:"id" bson:"_id"`
-		orderIndex int        `json:"order_index" bson:"order_index"`
-		engName string        `json:"eng_name" bson:"eng_name"`
-		trName  string        `json:"tr_name" bson:"tr_name"`
-		avatar  string        `json:"avatar" bson:"avatar"`
+		Id         bson.ObjectId `bson:"_id"`
+		orderIndex int           `bson:"orderindex"`
+		engName    string        `bson:"engname"`
+		trName     string        `bson:"trname"`
+		avatar     string        `bson:"avatar"`
 	}
 )
